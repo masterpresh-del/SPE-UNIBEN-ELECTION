81ffc7f472040fbfc164060e695483236ad3a8ac
masterpresh-del <osemudiamenmonday2@gmail.com>
2025-09-19 00:08:00 +0100

    Update settings.py

--- spe_election/settings.py
+++ spe_election/settings.py
@@ -20,12 +20,21 @@ MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','django.contrib.se
 'django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware',
 'django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
 ROOT_URLCONF = 'spe_election.urls'
-TEMPLATES = [{
-    'BACKEND':'django.template.backends.django.DjangoTemplates',
-    'DIRS':[BASE_DIR / 'templates'],
-    'APP_DIRS': True,
-    'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}
-}]
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # this points to the root templates folder
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 WSGI_APPLICATION = 'spe_election.wsgi.application'
 DATABASES = {'default': {'ENGINE':'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3'}}
 AUTH_PASSWORD_VALIDATORS = []
