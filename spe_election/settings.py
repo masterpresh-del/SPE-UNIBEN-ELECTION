7cecd169b56838862677a45eb9742e36cff30bbd
masterpresh-del <osemudiamenmonday2@gmail.com>
2025-09-18 15:07:40 +0100

    Add files via upload

---
+++ spe_election/settings.py
@@ -0,0 +1,43 @@
+
+import os
+from pathlib import Path
+from django.urls import reverse_lazy
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-dev-secret')
+DEBUG = os.environ.get('DEBUG','1') == '1'
+ALLOWED_HOSTS = ['*']
+INSTALLED_APPS = [
+    'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
+    'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
+    'election',
+]
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware',
+' django.middleware.common.CommonMiddleware'.strip(),
+'django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
+ROOT_URLCONF = 'spe_election.urls'
+TEMPLATES = [{
+    'BACKEND':'django.template.backends.django.DjangoTemplates',
+    'DIRS':[BASE_DIR / 'templates'],
+    'APP_DIRS': True,
+    'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}
+}]
+WSGI_APPLICATION = 'spe_election.wsgi.application'
+DATABASES = {'default': {'ENGINE':'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3'}}
+AUTH_PASSWORD_VALIDATORS = []
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Africa/Lagos'
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+LOGIN_URL = reverse_lazy('login')
+# Email (SMTP) settings via env
+EMAIL_HOST = os.environ.get('SMTP_HOST','')
+EMAIL_PORT = int(os.environ.get('SMTP_PORT') or 587)
+EMAIL_HOST_USER = os.environ.get('SMTP_USER','')
+EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASS','')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM','no-reply@example.com')
