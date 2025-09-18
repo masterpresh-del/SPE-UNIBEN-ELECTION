f43160cfa0d4256ef9bb2c61d37ade37304c1891
masterpresh-del <osemudiamenmonday2@gmail.com>
2025-09-18 17:56:56 +0100

    Update settings.py

--- spe_election/settings.py
+++ spe_election/settings.py
@@ -6,7 +6,9 @@ from django.urls import reverse_lazy
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-dev-secret')
 DEBUG = os.environ.get('DEBUG','False') == 'True'
-DATABASES = {"default": dj
+DATABASES = {
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
